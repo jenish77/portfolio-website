758497171170d2b32491144cfdba2bccb033539a
Jenish maru <jenish.m@elaunchinfotech.in>
2023-12-02 11:45:32 +0530

    create about section

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React from "react";
 import Image from "next/image";
 
@@ -49,11 +49,19 @@ const HeroSection = () => {
         </div>
         <div className="col-span-5 place-self-center mt-4 lg:mt-0">
           <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+            {/* <Image
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              src="/images/hero-image.avif"
+              width="300"
+              height="300"
+            /> */}
             <Image
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
               src="/images/hero-image.avif"
               width="300"
               height="300"
+              style={{ borderRadius: "50%" }}
+              alt="Hero Image"
             />
           </div>
         </div>
