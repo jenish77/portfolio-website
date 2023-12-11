3ea06699b0057d92dba32242bce8526b6ab567cd
Jenish maru <jenish.m@elaunchinfotech.in>
2023-12-11 18:01:18 +0530

    add socials

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -1,5 +1,9 @@
 import React from "react";
-import GithubIcon from "../../../public/images/github"
+import GithubIcon from "../../../public/git.svg"
+import Linkedincon from "../../../public/linkedin.svg"
+import Link from "next/link";
+import Image from "next/image";
+
 
 const EmailSection = () => {
   return (
@@ -12,7 +16,20 @@ const EmailSection = () => {
           Whether you have a question or just want to say hi, I'll try my best
           to get back to you!
         </p>
-        <div className="socials flex flex-row gap-2"></div>
+        <div className="socials flex flex-row gap-2">
+          <Link href="github.com">
+            <Image src={GithubIcon} alt="Github Icon"/>
+          </Link>
+          <Link href="Linkedin.com">
+            <Image src={Linkedincon} alt="Linkedin Icon"/>
+          </Link>
+        </div>
+      </div>
+      <div>  
+        <form className="flex flex-col gap-4">
+          <label htmlFor="email" className="text-white block mb-2 text-sm font-medium ">Your email</label>
+          <input type="email" id="email" required placeholder="jenish@gmail.com"/>
+        </form>
       </div>
     </section>
   );
