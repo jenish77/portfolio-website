aa19b5ef3432a63ea305ac86121f5293a3f3466e
Jenish maru <jenish.m@elaunchinfotech.in>
2023-12-14 10:32:55 +0530

    add cv

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -9,8 +9,8 @@ const HeroSection = () => {
     <section className="lg:py-16">
       <div className="grid grid-cols-1 sm:grid-cols-12">
         <motion.div
-          initial={{ opacity: 0,scale:0.5 }}
-          animate={{ opacity: 1,scale:1 }}
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.5 }}
           className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
         >
@@ -47,15 +47,19 @@ const HeroSection = () => {
             </button>
             <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white  mt-3">
               <span className="block bg-[#121212] hover:bg-slate-500 rounded-full px-5 py-2">
-                Download CV
+                <a href="/test.pdf" download="test.pdf">
+                  Download CV
+                </a>
               </span>
             </button>
           </div>
         </motion.div>
         <motion.div
-          initial={{ opacity: 0,scale:0.5 }}
-          animate={{ opacity: 1,scale:1 }}
-          transition={{ duration: 0.5 }} className="col-span-4 place-self-center mt-4 lg:mt-0">
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className="col-span-4 place-self-center mt-4 lg:mt-0"
+        >
           <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             {/* <Image
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
